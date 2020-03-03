f6f9a36127c286802fe82b6407a84b3ca1467257
sujatha.srideve <sujatha.srideve@LE-PF1EPTKS.ad.npspot.com>
2020-03-03 22:19:36 +0200

    Second commit

--- AmazonTest/src/main/java/com/amazon/qa/pages/HomePage.java
+++ AmazonTest/src/main/java/com/amazon/qa/pages/HomePage.java
@@ -3,5 +3,6 @@ package com.amazon.qa.pages;
 import com.amazon.qa.base.TestBase;
 
 public class HomePage extends TestBase {
-
+	//Page Repository: OR
+	// To be added
 }
--- AmazonTest/src/main/java/com/amazon/qa/pages/SearchPage.java
+++ AmazonTest/src/main/java/com/amazon/qa/pages/SearchPage.java
@@ -3,5 +3,8 @@ package com.amazon.qa.pages;
 import com.amazon.qa.base.TestBase;
 
 public class SearchPage extends TestBase{
+	
+	//Page Repository: OR
+	// To be added
 
 }
